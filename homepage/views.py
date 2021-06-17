6daafdfd19654ad7fe738c01f4d967252e6ba2b3
Airon Herbert <aironherbertms@gmail.com>
2021-06-17 15:36:13 -0300

    Experiências profissionais

--- homepage/views.py
+++ homepage/views.py
@@ -1,5 +1,14 @@
 from django.shortcuts import render
-
 # Create your views here.
 def index(request):
-    return render(request, 'home.html')
+    context={
+        'jobs': {
+            1: {
+                'title': 'Pesquisador Científico',
+                'company': 'Instituto Federal de Goiás',
+                'date': 'Novembro 2018  - Janeiro 2021',
+                'description': 'Desenvolvimento de softwares, relatórios, artigos científicos e manuais na área de energias renováveis e engenharia econômica aplicada para um projeto em parceria com a Enel.'
+            }
+        }
+    }
+    return render(request, 'home.html', context)
